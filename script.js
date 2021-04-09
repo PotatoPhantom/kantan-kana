18a4e5643a1ac8e6e574d4aa604e393377094010
PotatoPhantom <54708020+PotatoPhantom@users.noreply.github.com>
2021-04-09 06:55:13 -0700

    cookie fix

--- script.js
+++ script.js
@@ -198,7 +198,6 @@ function updateTable(kana_character) {
 }
 
 function setCookie(cname, cvalue, exdays) {
-  return false;
   var d = new Date();
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
   var expires = "expires=" + d.toUTCString();
